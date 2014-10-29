8fc61b377d61f61c74f1915cbd7f35e5b8e9d7e3
untxi <samarburola@gmail.com>
2014-10-29 00:03:18 -0600

    limpieza

--- Recursos/DATA_lettersAsm.asm
+++ Recursos/DATA_lettersAsm.asm
@@ -26,3 +26,99 @@ calendario15 db 'CCCCCCCCCCCCCCCCCCCC', 0
 ;Tag: Viernes
 ;Tag: Sábado
 ;Tag: Domingo
+LLL                                                      
+LLL                                                      
+LLL                                                      
+LLL                                                      
+LLL                                                      
+LLL UUUUUU    UUU  NNNNNN NNNNN      EEEEEEE   SSSSSS   
+LLL    UUU    UUU    NNNNNNNNNNN    EEE  EEEE SSS   SSS  
+LLL     UUU    UUU    NNNNNNN NNN    EEE  EEEE SSS   SSS  
+LLL     UUU   UUUU    NNNNN   NNN   EEE EEEEE  SSSSS      
+LLL     UUU   UUUU    NNNN    NNN  EEEE         SSSSSSS   
+LLL    UUUU  UUUUU    NNNN    NNN  EEEE             SSSS  
+LLLL   UUUUUUUUUUU    NNN     NNN   EEEE    EE SS    SSS  
+LLLLL   UUUUU6 UUUUU NNNN     NNNNN  EEEEEEEE SSSSSSSSS   
+
+                                        TT                   
+                                        TT                   
+                                       TTT                   
+MMMM MMMM MMMMM   000000  RRRR RRRR  TTTTTTT   EEEEEE   SSSSSS
+MMMMM MMMM MM0   00  200   RRRRR      TTT     EE  EEE  SSS   
+MMMM  MMM  MMM       200   RRRR       TT     EEEEEE     SSSS   
+MMMM  MMM  MMM   000 005   RRR        TT     EE           SSSS 
+MMM   MM   MMM  00  0002   RR         TTT    EEE            SSS
+MMM   MM   MMMM 0000020000 RR          TTTTT  EEEEEE   SSSSSSS 
+
+                                                  LL                  
+                                                  LL                  
+                        EE                        LL                  
+               III      EE                        LL               
+                       E                          LL                  
+MMM MMM  MMM   II    EEEE  RRR RRR  CCCC   OOOO   LL    EEEE   SSSS    
+ MMMM MMMMMMM  II  EE  EEE  RRRR  CCC     OO  OO  LL  EE  EE SS      
+ MMM  MMM MM   II  EEEEE    RRR   CC     OO   OO  LL EEEEEE   SSSS     
+ MMM  MM  MM   II  EE       RR    CC     OO   OO  LL EEE         SSS   
+ MM   MM  MM   II  EEEEEE   RR    CCCCCC  OOOOOO  LL  EEEEEE  SSSSS   
+
+
+
+   JJJ                                                      
+   JJJ                                                      
+                                                            
+                        EEE                   EEE        SSSSS
+  JJJJ  UUUU   UUU    EEE EEE  VVVV   VVV   EEE EEE    SSS SSSS 
+  JJJJ   UUU   UUU   EEE   EEE  VVV   VVV  EEE  EEE   SSS   SS  
+  JJJJ   UUU   UUU  EEEEEEEE    VVVV  VV  EEEEEEE     SSSSS    
+  JJJ    UUU  UUUU  EEE          VVV VV   EEE           SSSSS  
+  JJJ   UUU  UUUUU  EEEE         VVVVV    EEEE            SSSS 
+  JJJ    UUUUUUUUU   EEEEEEEE    VVVV      EEEEEEEE  SSSS SSS  
+  JJJ     UUU   UUUU   EEEE       VV         EEEE      SSSS    
+  JJJ                                                       
+  JJ                                                        
+ JJ                                                         
+                                                            
+
+             00                                                     
+            2005                                                    
+                                                                    
+       200                                                          
+ 2000   002 000    000 000  000200002200 000000    000 0002000  000 
+  000   00  000   002  000  00000    200205 000   002  0002002  000 
+  2002  00  000  000 0000   0000     20000  000   00 0000  00002    
+   000 00   000  000        000      2000   000  200         000005 
+   00000    000  000        000      0002   005  200            000 
+   2000     000  20000 000  005      000    000   0000 000 002  000 
+    00      2000    0005                    2002    0005    00005   
+                                                                    
+
+                       002                           000            
+                       00                           2005            
+                00    200                           2002            
+               000    200                           000             
+              000     200                           000             
+             00       000                           000             
+    0000     0000     000  2000      000         000000      0002   
+ 200  000  00  2000   0000000005  002 2000    000   002   200  0002 
+ 000       00   000   00000  000  000  000   000   2002  200    000 
+  000002     000000   000    200     00000  000    0002  000    000 
+     0000  00  2002   000    002  000 2002  000   0000   000    000 
+ 002  200 002 00005   000   000  000 00005  0000000 002  2000  200  
+ 0000000  00000 00000000000000   00000 00000 00005  00005 0000000   
+                                                                    
+
+        00                                                          
+       202                                                          
+       00                                                           
+       00                           00                              
+       00                                                           
+   000000    20000 0000 000  000    00 0000 0000    0000500   0000  
+  00   00   00  000  000020000000  200   0000 00   00  002  00   00 
+ 00   000  002   00  000  000 200  200   000  00  000   05  00   005
+200  2000  002   00  002  002 200  200   002  00   00  00  200   002
+ 00 20 00  000   0   00   00  005  200   00   00   00002    00   00 
+ 2000  0000 00000    00   02  2000  000  02   0000 00000     0000   
+                                                  000 00005         
+                                                 00     200         
+                                                 200   200          
+                                                   0000 
